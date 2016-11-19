0f926ac6ca3b14b8365de9b5c7407d1bd02e9d0a
Hayri Bakici <hbi@medicuja.com>
2016-11-19 01:20:58 +0100

    - adds first spiral drawing

--- lib/src/main/java/eu/bakici/imageprogressbar/indicator/SpiralBlockIndicator.java
+++ lib/src/main/java/eu/bakici/imageprogressbar/indicator/SpiralBlockIndicator.java
@@ -4,16 +4,34 @@ import eu.bakici.imageprogressbar.utils.IndicatorUtils;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static android.graphics.Color.*;
 
 public class SpiralBlockIndicator extends BlockIndicator {
 
     Point center;
 
+    int[] colors = {
+            BLACK, BLUE, RED, YELLOW, MAGENTA, CYAN, DKGRAY, GRAY, GREEN,
+            LTGRAY, WHITE
+    };
+
+    int colorIndex = 0;
+
+    private int getNextColor() {
+        Random random = new Random();
+        return colors[random.nextInt(colors.length)];
+    }
 
 
     @Override
@@ -28,40 +46,60 @@ public class SpiralBlockIndicator extends BlockIndicator {
         final int numberOfDivisions = numberOfRows / 2;
 
 
-        for (int i = 0; i < mBlockSum; i++) {
-            final int col = i % numberOfCols;
-            final int row = i / numberOfCols;
+//        Paint paint = new Paint();
+//        for (int i = 0; i < mBlockSum; i++) {
+//            final int col = i % numberOfCols;
+//            final int row = i / numberOfCols;
+//
+//            paint.setStyle(Paint.Style.STROKE);
+//            canvas.drawCircle(center.x, center.y, row * mPixels, paint);
+//
+//
+//
+//            if (row > numberOfCircles) {
+//                break;
+//            }
+//        }
 
-            Paint paint = new Paint();
-            paint.setStyle(Paint.Style.STROKE);
-            canvas.drawCircle(center.x, center.y, row * mPixels, paint);
+        float x0 = center.x;
+        float y0 = center.y;
+        for (int i = 0; i < mPixels * 360; i++) {
+            double angle = 0.1 * i;
+            float x = (float) (center.x + (10 * angle) * Math.cos(angle));
+            float y = (float) (center.y + (10 * angle) * Math.sin(angle));
 
-
-
-            if (row > numberOfCircles) {
-                break;
-            }
+            canvas.drawLine(x0, y0, x, y, new Paint());
+            x0 = x;
+            y0 = y;
         }
-        Path path = new Path();
-        int lastTop = 0;
-        int lastLeft = 0;
-        int lastRight = center.x;
-        int lastBottom = center.y;
-        for (int i = 0; i < numberOfCols; i++) {
-            int angle = i * (360 / numberOfCols);
-            final int col = i % numberOfCols;
-            int x = (int) (center.x + (col * mPixels) * Math.cos(Math.toRadians(angle)));
-            int y = (int) (center.y + (col * mPixels) * Math.sin(Math.toRadians(angle)));
 
-
-            canvas.drawLine(center.x, center.y, x, y, new Paint());
-            RectF oval = new RectF(x, y, lastRight, lastBottom);
-            final Paint rectPaint = new Paint();
-            rectPaint.setStyle(Paint.Style.STROKE);
-            canvas.drawRect(oval, rectPaint);
-
-
-        }
+//        canvas.drawPath(path, new Paint());
+//
+//        int lastAngle = 0;
+//        for (int i = 1; i <= numberOfCols; i++) {
+//            int angle = i * (360 / numberOfCols);
+//
+//            for (int j = lastAngle; j < angle; j++) {
+//                int x1 = (int) (center.x + (i * mPixels) * Math.cos(j));
+//                int y1 = (int) (center.y + (i * mPixels) * Math.sin(j));
+//                Log.d("SpiralBlockIndicator", String.format("j = %s, lastAngle = %s, angle = %s, x1 = %s, y1 = %s", j, lastAngle, angle, x1, y1));
+//                canvas.drawPoint(x1, y1, new Paint());
+//            }
+////            int x = (int) (center.x + (i * mPixels) * Math.cos(Math.toRadians(angle)));
+////            int y = (int) (center.y + (i * mPixels) * Math.sin(Math.toRadians(angle)));
+//            lastAngle = angle;
+//
+//            Log.d("SpiralBlockIndicator", String.format("i = %s, angle = %s", i, angle));
+//
+////            canvas.drawPoint();
+////            canvas.drawLine(center.x, center.y, x, y, new Paint());
+////            RectF oval = new RectF(x, y, lastRight, lastBottom);
+////            final Paint rectPaint = new Paint();
+////            rectPaint.setStyle(Paint.Style.STROKE);
+////            canvas.drawRect(oval, rectPaint);
+//
+//
+//        }
 
         mPreBitmap = bitmap;
         mCurrentBitmap = bitmap;
